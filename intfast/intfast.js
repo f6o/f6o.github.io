07cde63ec1e543079d585742b92b324732c80e02
f6o <fluffy6h@gmail.com>
2019-11-09 03:28:46 +0000

    show some info

--- intfast/intfast.js
+++ intfast/intfast.js
@@ -4,19 +4,48 @@ class IfLog {
         this.start_time = Date.now()
         this.end_time = -1
     }
-    serialize() {
-        return JSON.stringify(this)
+    setStartTime(t) {
+        this.start_time = t
+    }
+    finish() {
+        this.end_time = Date.now()
+    }
+    toString() {
+//        return JSON.stringify(this)
+        let r = this.remainings()
+        return `fasting since ${ new Date(this.start_time) } and ${ r / 1000 / 60 / 60 } hours to go`
+    }
+    diff() {
+        return this.start_time - Date.now()
+    }
+    remainings() {
+        switch( this.type ) {
+        case '16:8':
+            return this.diff() + 16 * 60 * 60 * 1000
+            break;
+        default:
+            break;
+        }
     }
 }
 
-var l0 = new IfLog('16:8')
-
 Vue.component('current', {
-	template: `<p>current: {{l0}}</p>`
+    data: function () {
+        var l = new IfLog('16:8')
+        l.setStartTime(1573198200 * 1000)
+        return { iflog: l }
+    },
+	template: `<div>
+<p>fasting since {{ new Date(iflog.start_time) }}</p>
+<p>rems: {{ iflog.remainings() / 1000 / 60 }} mins</p>
+<p>now {{ Date.now() }} </p>
+</div>
+
+`
 })
 
 Vue.component('history', {
 	template: `<p>history: todo</p>`
-	})
+})
 
 new Vue({ el: '#components'})
