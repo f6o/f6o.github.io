04287dcadfb40bd9fa383db3464ab9424ea96739
f6o <fluffy6h@gmail.com>
2019-11-09 02:18:16 +0000

    define IfLog format

--- intfast/intfast.js
+++ intfast/intfast.js
@@ -1,6 +1,19 @@
+class IfLog {
+    constructor(type) {
+        this.type = type
+        this.start_time = Date.now()
+        this.end_time = -1
+    }
+    serialize() {
+        return JSON.stringify(this)
+    }
+}
+
+var l0 = new IfLog('16:8')
+
 Vue.component('current', {
-	template: `<p>current: todo</p>`
-	})
+	template: `<p>current: {{l0}}</p>`
+})
 
 Vue.component('history', {
 	template: `<p>history: todo</p>`
