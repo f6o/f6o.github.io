8ae33592ae2340b37ed1e606263c99fc9ae04a33
f6o <fluffy6h@gmail.com>
2019-11-16 10:40:28 +0900

    clean

--- intfast/intfast.js
+++ intfast/intfast.js
@@ -1,66 +1,16 @@
-class IfLog {
-    constructor(type) {
-        this.type = type
-        this.start_time = Date.now()
-        this.end_time = -1
-    }
-    setStartTime(t) {
-        this.start_time = t
-    }
-    finish() {
-        this.end_time = Date.now()
-    }
-    toString() {
-        return JSON.stringify(this)      
-    }
-    diff() {
-        return this.start_time - Date.now()
-    }
-    remainings() {
-        switch( this.type ) {
-        case '16:8':
-            return this.diff() + 16 * 60 * 60 * 1000
-            break;
-        default:
-            break;
-        }
-    }
-    saveToLocalStorage(lskey) {
-        if ( localStorage.getKey(lskey) == null ) {
-            localStorage.setKey(lskey, this.toString())
-        }
-    }
-}
-
-function loadFromLocalStorage(lskey) {
-    // TODO: multiple constructors are not alloed?
-    //var ifls = JSON.parse(localStorage.getKey(lskey))
-    //return (new IfLog(ifls.type, ifls.start_time, ifls.end_time))
-}
-
-Vue.component('current', {
+Vue.component('logs', {
     data: function () {
-        var l = new IfLog('16:8')
-        
-        // REMOVE: test code
-        l.setStartTime(1573198200 * 1000)
-        
-        // TODO: usable?
-        //l.saveToLocalStorage('l0')
-
-        return { iflog: l }
+        return {
+            logs: [ 1, 2, 3 ]
+        }
     },
     // TODO: sample components
 	template: `<div>
-<p>fasting since {{ (new Date(iflog.start_time)).toUTCString() }}</p>
-<p>rems: {{ iflog.remainings() / 1000 / 60 }} mins</p>
-<p>now {{ Date.now() }} </p>
-</div>
-`
-})
-
-Vue.component('history', {
-    template: `<p>history: todo</p>`
+<ul v-if="logs.length">
+<li v-for="l in logs">{{ l }}</li>
+</ul>
+<p v-else>No logs</p>
+</div>`
 })
 
 new Vue({ el: '#components'})
