927ac1b331dbf23aa18c3e0d1a28f585cea5ee0c
f6o <fluffy6h@gmail.com>
2019-11-16 11:44:34 +0900

    create new component

--- intfast/intfast.js
+++ intfast/intfast.js
@@ -4,13 +4,21 @@ Vue.component('logs', {
             logs: [ 1, 2, 3 ]
         }
     },
-	template: `<div class="card-deck">
+    template: `<div class="container-fluid"><div class="row"><div class="col-sm-12"><div class="card-deck">
 <div class="card" v-for="l in logs">
 <div class="card-body">
 <a href="#">{{ l }}</a>
 </div>
 </div>
-</div>`
+</div></div></div></div>`
 })
 
-new Vue({ el: '#components'})
+Vue.component('control',{
+    template : `<header class="navbar">
+      <div id="controls">
+        <button>click here</button>
+      </div>
+    </header>`
+})
+
+new Vue({ el: '#app'})
