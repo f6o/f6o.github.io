209fb854518a00d44ee1055001a32e1fa0788222
f6o <fluffy6h@gmail.com>
2022-08-11 21:08:11 +0900

    fix

--- apps/cal/main.js
+++ apps/cal/main.js
@@ -8,32 +8,11 @@ var vm = new Vue({
     },
     methods: {
         add: function () {
-            let kcal = document.getElementById('kcal').valueAsNumber
-            console.log(this)
-            if ( kcal > 0 ) {
-                let item = { ts: Date.now(), kcal: kcal }
-                console.log(item)
+            let kcal = parseInt(this.form_kcal)
+            if (kcal > 0 && kcal < 2000) {
+                let item = { ts: Date.now(), kcal: kcal, count: Math.ceil(kcal / this.unit) }
                 this.items.push(item)
             }
-        }
-    },
-    computed: {
-        inTwentyFourHours: function () {
-            const now = Date.now()
-            // use filter?
-            const l = this.items.filter((item) => {
-                const diff = now - item.ts
-                return diff > 0 && diff < 1000 * 60 * 60 * 24
-            }).map((item)=>{
-                item.count = Math.ceil(item.kcal / this.unit)
-                return item
-            })
-            const sum = l.reduce((s, item) => {
-                return item.kcal + s
-            }, 0)
-            console.log(l)
-            console.log(sum)
-            return { list: l, sum: sum }
         }
     }
 })
