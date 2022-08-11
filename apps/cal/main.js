b641d270e774287ebb858e1525dc0641c94e09d7
f6o <fluffy6h@gmail.com>
2022-08-11 09:30:02 +0900

    list humbergers

--- apps/cal/main.js
+++ apps/cal/main.js
@@ -1,6 +1,25 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        greetings: 'Hello'
+        unit: 10,
+        items: [
+            { ts: 1660175983818, count: 30 },
+            { ts: 1660145983818, count: 20 },
+            { ts: 1659015983818, count: 10 }
+        ]
+    },
+    computed: {
+        inTwentyFourHours: function() {
+            const now = Date.now()
+            const l = []
+            // use filter?
+            for ( let i = 0; i < this.items.length; i++ ) {
+                const diff = now - this.items[i].ts
+                if ( diff > 0 && diff < 1000*60*60*24 ) {
+                    l.push(this.items[i])
+                }
+            }
+            return l
+        }
     }
 })
