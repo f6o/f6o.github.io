9acd7ccc21e3e20d48f77293f5b01cb9ca73a6d0
f6o <fluffy6h@gmail.com>
2022-08-11 09:41:23 +0900

    show warning

--- apps/cal/main.js
+++ apps/cal/main.js
@@ -1,25 +1,30 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        unit: 10,
+        unit: 100,
+        limit: 1600,
         items: [
-            { ts: 1660175983818, count: 30 },
-            { ts: 1660145983818, count: 20 },
-            { ts: 1659015983818, count: 10 }
+            { ts: 1660175983818, kcal: 1000 },
+            { ts: 1660145983818, kcal: 400 },
+            { ts: 1659015983818, kcal: 1200 }
         ]
     },
     computed: {
-        inTwentyFourHours: function() {
+        inTwentyFourHours: function () {
             const now = Date.now()
             const l = []
+            let sum = 0
             // use filter?
-            for ( let i = 0; i < this.items.length; i++ ) {
+            for (let i = 0; i < this.items.length; i++) {
                 const diff = now - this.items[i].ts
-                if ( diff > 0 && diff < 1000*60*60*24 ) {
-                    l.push(this.items[i])
+                if (diff > 0 && diff < 1000 * 60 * 60 * 24) {
+                    const item = this.items[i]
+                    item.count = item.kcal / this.unit
+                    l.push(item)
+                    sum += item.kcal
                 }
             }
-            return l
+            return { list: l, sum: sum }
         }
     }
 })
